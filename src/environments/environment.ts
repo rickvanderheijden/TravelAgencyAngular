88fee42746edb0aa263ca23a9c75ff35e65fd3ac
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2019-03-15 16:50:18 +0100

    coverage

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,7 +6,6 @@
 export const environment = {
   server: 'http://localhost:9000',
   url: 'http://localhost:9000',
-  // server: 'http://localhost:8080',
-  // url: 'http://localhost:8080',
+  CODACY_PROJECT_TOKEN: '98c0914fde984c8a8f7b768264df5d16',
   production: false
 };
