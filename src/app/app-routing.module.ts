24c8cf3cb54fba3517a1fcc5beed02b6110f6ae8
RoyTimmers <roytimmers_7@hotmail.com>
2019-03-18 21:14:51 +0100

    Schoonmaken

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes, PreloadAllModules } from '@angular/router';
 
 import { FullLayoutComponent } from './layouts/full/full-layout.component';
-import { ContentLayoutComponent } from './layouts/content/content-layout.component';
 
 import { Full_ROUTES } from './shared/routes/full-layout.routes';
-import { CONTENT_ROUTES } from './shared/routes/content-layout.routes';
-import {PUBLIC_ROUTES} from './shared/routes/public.routes';
 
 import {AuthLoginComponent} from './auth/auth-login.component';
-import {AuthGuardService} from './auth/auth.guard';
-import {HomepageComponent} from './pages/public/homepage/homepage.component';
 import {MapsComponent} from './maps/maps.component';
 
 
