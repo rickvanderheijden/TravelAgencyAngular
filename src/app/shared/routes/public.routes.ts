f31a8d1d6bed9f349390f4de2fd862199c37fbe3
Koen Sengers <k.sengers@gynzy.com>
2019-03-22 12:44:20 +0100

    Fix bugs

--- src/app/shared/routes/public.routes.ts
+++ src/app/shared/routes/public.routes.ts
@@ -1,9 +1,10 @@
 import { Routes, RouterModule } from '@angular/router';
+import {HomepageModuleModule} from '../../pages/public/homepage/homepage.module';
 
-//Route for content layout with sidebar, navbar and footer
+// Route for content layout with sidebar, navbar and footer
 export const PUBLIC_ROUTES: Routes = [
   {
-    path: '',
-    loadChildren: '../../pages/public/homepage/homepage.module#HomepageModuleModule'
+    path: 'home',
+    loadChildren: './pages/public/homepage/homepage.module#HomepageModuleModule'
   }
 ];
