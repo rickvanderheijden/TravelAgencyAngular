77f10b3020a93795cda3f07f713533943154bc87
Koen Sengers <k.sengers@gynzy.com>
2019-04-27 12:34:16 +0200

    Fix mock call get trips

--- src/app/pages/trip/index/trip.component.spec.ts
+++ src/app/pages/trip/index/trip.component.spec.ts
@@ -1,8 +1,17 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TripComponent } from './trip.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {HttpClientModule} from '@angular/common/http';
+import {Observable, of} from 'rxjs';
+import {Trip} from '../../../../models/trip';
+import {TripService} from '../../../services/trip.service';
+
+class TestTrips {
+  getTrips(): Observable<Array<Trip>> {
+    return of(new Array<Trip>())
+  }
+}
 
 describe('TripComponent', () => {
   let component: TripComponent;
@@ -11,6 +20,7 @@ describe('TripComponent', () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       declarations: [ TripComponent ],
+      providers: [{provide: TripService, useValue: new TestTrips()}],
       schemas: [NO_ERRORS_SCHEMA],
       imports: [
         HttpClientModule
