ae41622718bad0a957088d796c4396d116916334
Arthur Doorgeest <arthur.doorgeest@outlook.com>
2019-05-04 12:46:58 +0200

    mad some changes for trip item

--- src/app/pages/trip-item/create/trip-item-create.module.ts
+++ src/app/pages/trip-item/create/trip-item-create.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import {TripItemCreateComponent} from './trip-item-create.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {NgSelectModule} from '@ng-select/ng-select';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     ReactiveFormsModule,
-    NgbModule
+    NgbModule,
+    NgSelectModule
   ],
   declarations: [
     TripItemCreateComponent,
