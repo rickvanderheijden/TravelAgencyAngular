8a912ee523e7f621cfcdfb7d4aa1abc2993d7079
ArthurDoorgeest <31449856+ArthurDoorgeest@users.noreply.github.com>
2019-05-07 21:19:32 +0200

    fixed form group error

--- src/app/pages/trip/trip-update/trip-update.component.ts
+++ src/app/pages/trip/trip-update/trip-update.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {TripService} from '../../../services/trip.service';
 import {Trip} from '../../../../models/trip';
 import {ActivatedRoute, Router} from '@angular/router';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-trip-update',
@@ -9,14 +10,17 @@ import {ActivatedRoute, Router} from '@angular/router';
   styleUrls: ['./trip-update.component.scss']
 })
 export class TripUpdateComponent implements OnInit {
+  tripUpdateForm: FormGroup;
   trip: Trip;
   tripId: any;
   loading = false;
 
   constructor(private tripService: TripService, private route: ActivatedRoute, private router: Router) {
     this.tripService.getById(route.snapshot.params.id).subscribe((data: any) => {
-      this.trip = data;
+      this.trip = new Trip(data);
+      console.log(this.trip);
       this.tripId = route.snapshot.params.id;
+      this.setForm();
     });
   }
 
@@ -34,5 +38,16 @@ export class TripUpdateComponent implements OnInit {
 
   back() {
     this.router.navigate(['/trip/']);
+  }
+
+  setForm() {
+    this.tripUpdateForm = new FormGroup({
+      name: new FormControl(this.trip.name, [Validators.minLength(4), Validators.required]),
+      description: new FormControl(this.trip.description),
+      summary: new FormControl(this.trip.summary),
+      imageUrl: new FormControl(this.trip.imageUrl, [Validators.minLength(6), Validators.email, Validators.required]),
+      totalPrice: new FormControl(this.trip.totalPrice, [Validators.minLength(5)]),
+      discount: new FormControl(this.trip.discount)
+    });
   }
 }
