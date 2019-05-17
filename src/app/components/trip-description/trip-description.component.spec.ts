a2131e2376858d1177fb6fa2671842161562e4f0
Rick van der Heijden <rick.van.der.heijden@philips.com>
2019-05-17 10:57:45 +0200

    Fix test

--- src/app/components/trip-description/trip-description.component.spec.ts
+++ src/app/components/trip-description/trip-description.component.spec.ts
@@ -3,8 +3,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { TripDescriptionComponent } from './trip-description.component';
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {Trip} from '../../../models/trip';
+import {Travel} from '../../../models/travel';
+import {TripItem} from '../../../models/TripItem';
+import {HttpClientModule} from '@angular/common/http';
+import {JwtModule} from '@auth0/angular-jwt';
+import {BsModalService, ModalModule} from 'ngx-bootstrap';
 
-class TestTrip { name = 'Test trip'; }
+class TestTrip { name = 'Test trip'; imageUrl = 'Image URL'; description = 'Description'; }
+class TestTravel { name = 'Test travel'; totalPrice = 1500; tripItems = new Array<TripItem>(); }
 
 describe('TripDescriptionComponent', () => {
   let component: TripDescriptionComponent;
@@ -14,6 +20,15 @@ describe('TripDescriptionComponent', () => {
     TestBed.configureTestingModule({
       declarations: [ TripDescriptionComponent ],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [BsModalService],
+      imports: [
+        HttpClientModule,
+        ModalModule.forRoot(),
+        JwtModule.forRoot({
+          config: {
+            tokenGetter: () => { return 'testtoken'; }
+          }})
+      ]
     })
     .compileComponents();
   });
@@ -22,6 +37,7 @@ describe('TripDescriptionComponent', () => {
     fixture = TestBed.createComponent(TripDescriptionComponent);
     component = fixture.componentInstance;
     component.trip = new Trip(new TestTrip);
+    component.travel = new Travel(new TestTravel);
     fixture.detectChanges();
   });
 
